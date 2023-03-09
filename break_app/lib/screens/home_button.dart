04b982e5054990de4731332b19e3dc1b945db1fb
Wayne Tan <wtjh00@gmail.com>
2023-03-09 14:26:57 +0800

    chore: style timer running buttons

--- break_app/lib/screens/home_button.dart
+++ break_app/lib/screens/home_button.dart
@@ -5,25 +5,37 @@ class HomeButton extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Container(
+      padding: const EdgeInsets.fromLTRB(40, 50, 40, 10),
+      width: double.infinity,
       height: 400,
       child: Column(
         children: [
-          Row(children: [
-            GFButton(
-              onPressed: (){},
-              text:"primary"
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Container(
+              width: 150,
+              child: GFButton(
+                onPressed: (){},
+                text:"primary"
+              ),
             ),
-            GFButton(
-              onPressed: (){},
-              text:"primary"
+            Container(
+              width: 150,
+              child: GFButton(
+                onPressed: (){},
+                text:"primary"
+              ),
             ),
           ],),
-          Row(children: [
+          Container(
+            width: double.infinity,
+            child: 
             GFButton(
             onPressed: (){},
             text:"primary"
           ),
-          ],),
+          ),
         ],
       ),
     );
