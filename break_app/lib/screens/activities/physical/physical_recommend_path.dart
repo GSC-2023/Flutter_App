baab8113ef2337c7a9c9397a782e591580648950
Wayne Tan <wtjh00@gmail.com>
2023-03-21 03:26:29 +0800

    chore[WIP]: style physical_recommend_path

--- break_app/lib/screens/activities/physical/physical_recommend_path.dart
+++ break_app/lib/screens/activities/physical/physical_recommend_path.dart
@@ -2,6 +2,10 @@ import 'package:break_app/screens/activities/physical/components/amenityCard.dar
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:break_app/misc_utils/customDrawer.dart';
+import 'package:break_app/colors/colors.dart';
+import 'package:getwidget/getwidget.dart';
+
+
 
 class PhysicalRecommendPath extends StatefulWidget {
   @override
@@ -31,7 +35,9 @@ class _PhysicalRecommendPathState extends State<PhysicalRecommendPath> {
               TextStyle(fontWeight: FontWeight.bold, color: Color(0xff2E593F)),
         ),
         backgroundColor: Color(0xECEAEA),
-        foregroundColor: Color(0xECEAEA),
+        shadowColor: Color(0xECEAEA),
+        surfaceTintColor: Color(0xECEAEA),
+        foregroundColor:  Color(0xECEAEA),
       ),
       body: Container(
         child: Container(
@@ -40,6 +46,7 @@ class _PhysicalRecommendPathState extends State<PhysicalRecommendPath> {
             vertical: 20,
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -48,7 +55,7 @@ class _PhysicalRecommendPathState extends State<PhysicalRecommendPath> {
                     flex: 5,
                     child: Text(
                       "Intended Walking Duration: ",
-                      style: TextStyle(fontSize: 24),
+                      style: TextStyle(fontSize: 18),
                     ),
                   ), 
                   Flexible(
@@ -69,27 +76,55 @@ class _PhysicalRecommendPathState extends State<PhysicalRecommendPath> {
                 ],
               ),
               SizedBox(
-                height: 20,
+                height: 50,
               ),
-              ElevatedButton(
-                onPressed: () {}, //TODO logic to calculate route
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
+              // ElevatedButton(
+              //   onPressed: () {}, //TODO logic to calculate route
+              //   style: ElevatedButton.styleFrom(
+              //     shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.circular(30.0),
+              //     ),
+              //     backgroundColor: DarkGreen,
+              //   ),
+              //   child: Container(
+              //     alignment: Alignment.center,
+              //     padding: EdgeInsets.symmetric(vertical: 30),
+              //     width: double.infinity,
+              //     height: 70,
+              //     child: Text(
+              //       'Generate Amenities',
+              //       style: TextStyle(
+              //         fontSize: 20,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade500,
+                      offset: Offset(10.0, 10.0),
+                      blurRadius: 30.0
+                    )
+                  ]
                 ),
+                width: double.infinity,
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
                 child: Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.symmetric(vertical: 30),
-                  width: double.infinity,
-                  child: Text(
-                    'Generate Amenities',
-                    style: TextStyle(fontSize: 30),
+                  height: 50,
+                  child: GFButton(
+                    onPressed: () {},
+                    text: "Generate Amenities",
+                    textColor: White,
+                    shape: GFButtonShape.pills,
+                    color: DarkGreen,
                   ),
                 ),
               ),
               SizedBox(
-                height: 20,
+                height: 50,
               ),
               data.isEmpty
                   ? Text("no data!")
@@ -99,6 +134,9 @@ class _PhysicalRecommendPathState extends State<PhysicalRecommendPath> {
                         Text(
                           "List of Amenities:",
                           style: TextStyle(fontSize: 30),
+                        ),
+                        SizedBox(
+                          height: 20,
                         ),
                         for (var location in locations)
                           AmenityCard(
