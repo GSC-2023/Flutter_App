35adf47360cca08c51b3281396a72734c5fa896b
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-03-28 11:31:29 +0800

    social single loading ui and layout

--- break_app/lib/screens/social.dart
+++ break_app/lib/screens/social.dart
@@ -3,9 +3,11 @@ import 'package:break_app/screens/socialSingle.dart';
 import 'package:flutter/material.dart';
 import 'package:break_app/misc_utils/customDrawer.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import '../models/breakUser.dart';
 import '../models/profile.dart';
+import 'package:intl/intl.dart' show toBeginningOfSentenceCase;
 
 class PhotoItem {
   final String image;
@@ -114,144 +116,158 @@ class _SocialState extends State<Social> {
   TextEditingController searchNameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return loading
-        ? const Center(
-            child: CircularProgressIndicator(),
-          )
-        : Scaffold(
-            backgroundColor: Color.fromARGB(255, 231, 235, 227),
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              iconTheme: IconThemeData(color: Colors.black),
-              title: Text(
-                "Meet your friends",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff2E593F),
-                  fontSize: 30,
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 231, 235, 227),
+        drawer: CustomDrawer(),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            "Meet your friends",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color(0xff2E593F),
+              fontSize: 30,
+            ),
+          ),
+          backgroundColor: Color(0xECEAEA),
+          shadowColor: Color(0xECEAEA),
+          surfaceTintColor: Color(0xECEAEA),
+          foregroundColor: Color(0xECEAEA),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Column(
+            children: [
+              //SEARCH BAR
+              Form(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    Material(
+                      color: Colors.transparent,
+                      elevation: 10.0,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      child: TextFormField(
+                          controller: searchNameController,
+                          onFieldSubmitted: (value) =>
+                              searchFriend(value, context),
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                              hintText: 'Search',
+                              hintStyle: TextStyle(
+                                  color: Color.fromARGB(255, 27, 115, 97)),
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: Icon(Icons.search),
+                              prefixIconColor:
+                                  Color.fromARGB(255, 27, 115, 97))),
+                    ),
+                  ],
                 ),
               ),
-              backgroundColor: Color(0xECEAEA),
-              shadowColor: Color(0xECEAEA),
-              surfaceTintColor: Color(0xECEAEA),
-              foregroundColor: Color(0xECEAEA),
-            ),
-            body: Container(
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: Column(
-                children: [
-                  //SEARCH BAR
-                  Form(
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 20.0),
-                        Material(
-                          color: Colors.transparent,
-                          elevation: 10.0,
-                          shadowColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50.0),
+              loading
+                  ? Column(children: [
+                      SizedBox(
+                        height: 100,
+                      ),
+                      Container(
+                        height: 200,
+                        child: Center(
+                          child: SpinKitFadingCircle(
+                            color: Colors.green[200],
+                            size: 100.0,
                           ),
-                          child: TextFormField(
-                              controller: searchNameController,
-                              onFieldSubmitted: (value) =>
-                                  searchFriend(value, context),
-                              decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                    borderSide: BorderSide.none,
-                                    borderRadius: BorderRadius.circular(50.0),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide.none,
-                                    borderRadius: BorderRadius.circular(50.0),
-                                  ),
-                                  hintText: 'Search',
-                                  hintStyle: TextStyle(
-                                      color: Color.fromARGB(255, 27, 115, 97)),
-                                  fillColor: Colors.white,
-                                  filled: true,
-                                  prefixIcon: Icon(Icons.search),
-                                  prefixIconColor:
-                                      Color.fromARGB(255, 27, 115, 97))),
                         ),
-                      ],
-                    ),
-                  ),
-
+                      ),
+                    ])
+                  :
                   //GRID OF FRIENDS
                   Expanded(
                       child: Container(
-                    margin: EdgeInsets.only(top: 30),
-                    child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 1,
-                            mainAxisSpacing: 0,
-                            crossAxisSpacing: 20),
-                        itemCount: users.length,
-                        itemBuilder: (BuildContext ctx, index) {
-                          return Container(
-                              child: Column(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => SocialSingle(
-                                              user: user,
-                                              users: users[index],
-                                              deleteFriend: deleteFriend,
-                                              addFriend: addFriend)));
-                                },
-                                child: Material(
-                                  color: Colors.transparent,
-                                  child: Container(
-                                    margin: EdgeInsetsDirectional.symmetric(
-                                        vertical: 0),
-                                    height: 100,
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            width: 4,
-                                            color: Color.fromARGB(
-                                                255, 27, 115, 97)),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.black,
-                                            spreadRadius: 1,
-                                            blurRadius: 5,
-                                          )
-                                        ],
-                                        image: DecorationImage(
-                                          fit: BoxFit.fitHeight,
-                                          image:
-                                              NetworkImage(users[index].image),
-                                        ),
-                                        shape: BoxShape.circle),
+                      margin: EdgeInsets.only(top: 30),
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  childAspectRatio: 1,
+                                  mainAxisSpacing: 0,
+                                  crossAxisSpacing: 20),
+                          itemCount: users.length,
+                          itemBuilder: (BuildContext ctx, index) {
+                            return Container(
+                                child: Column(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => SocialSingle(
+                                                user: user,
+                                                users: users[index],
+                                                deleteFriend: deleteFriend,
+                                                addFriend: addFriend)));
+                                  },
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    child: Container(
+                                      margin: EdgeInsetsDirectional.symmetric(
+                                          vertical: 0),
+                                      height: 100,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              width: 4,
+                                              color: Color.fromARGB(
+                                                  255, 27, 115, 97)),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: Colors.black,
+                                              spreadRadius: 1,
+                                              blurRadius: 5,
+                                            )
+                                          ],
+                                          image: DecorationImage(
+                                            fit: BoxFit.fitHeight,
+                                            image: NetworkImage(
+                                                users[index].image),
+                                          ),
+                                          shape: BoxShape.circle),
+                                    ),
                                   ),
                                 ),
-                              ),
 
-                              // Spacer(flex: 1,),
+                                // Spacer(flex: 1,),
 
-                              Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: Text(
-                                  users[index].name,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromARGB(255, 27, 115, 97)),
-                                ),
-                                padding: EdgeInsets.all(0),
-                                alignment: Alignment.bottomCenter,
-                              )
-                            ],
-                          ));
-                        }),
-                  ))
-                ],
-              ),
-            ));
+                                Container(
+                                  margin: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text(
+                                    toBeginningOfSentenceCase(
+                                        users[index].name)!,
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color:
+                                            Color.fromARGB(255, 27, 115, 97)),
+                                  ),
+                                  padding: EdgeInsets.all(0),
+                                  alignment: Alignment.bottomCenter,
+                                )
+                              ],
+                            ));
+                          }),
+                    ))
+            ],
+          ),
+        ));
   }
 }
